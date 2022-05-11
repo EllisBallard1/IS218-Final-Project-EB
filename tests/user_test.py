c1794ded3433f33201c5c4ca5c5cae03210d16ab
Ellis Ballard <eab4@njit.edu>
2022-05-11 13:01:14 -0400

    User test

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,17 +1,17 @@
 import logging
 
 from app import db
-from app.db.models import User, Song
+from app.db.models import User, Transactions
 from faker import Faker
 
 def test_adding_user(application):
     log = logging.getLogger("myApp")
     with application.app_context():
         assert db.session.query(User).count() == 0
-        assert db.session.query(Song).count() == 0
+        assert db.session.query(Transactions).count() == 0
         #showing how to add a record
         #create a record
-        user = User('ellis@gmail.com', 'testtest')
+        user = User('ellis@gmail.com', 'passwordTesting')
         #add it to get ready to be committed
         db.session.add(user)
         #call the commit
@@ -24,22 +24,22 @@ def test_adding_user(application):
         #asserting that the user retrieved is correct
         assert user.email == 'ellis@gmail.com'
         #this is how you get a related record ready for insert
-        user.songs= [Song("MyTitle","Ellis","2022","Alternative"),Song("test2","te","2022","Rock")]
-        #commit is what saves the songs
+        user.transaction = [Transactions(500, 'CREDIT'), Transactions(-200, 'DEBIT')]
+        #commit is what saves the transactions
         db.session.commit()
-        assert db.session.query(Song).count() == 2
-        song1 = Song.query.filter_by(title='MyTitle').first()
-        assert song1.title == "MyTitle"
-        #changing the title of the song
-        song1.title = "spam"
-        #saving the new title of the song
+        assert db.session.query(Transactions).count() == 2
+        transaction1 = Transactions.query.filter_by(AMOUNT=500).first()
+        assert transaction1.AMOUNT == 500
+        #changing the amount of the transaction
+        transaction1.AMOUNT = 600
+        #saving the new transaction amount
         db.session.commit()
-        song2 = Song.query.filter_by(title='spam').first()
-        assert song2.title == "spam"
+        transaction2 = Transactions.query.filter_by(AMOUNT=-200).first()
+        assert transaction2.AMOUNT == -200
         #checking cascade delete
         db.session.delete(user)
         assert db.session.query(User).count() == 0
-        assert db.session.query(Song).count() == 0
+        assert db.session.query(Transactions).count() == 0
 
 
 
