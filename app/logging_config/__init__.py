61cf90472f82d9a75be542bc658d193a7e5df004
Ellis Ballard <eab4@njit.edu>
2022-05-11 18:56:28 -0400

    Added debug logger and handler

--- app/logging_config/__init__.py
+++ app/logging_config/__init__.py
@@ -101,6 +101,13 @@ LOGGING_CONFIG = {
             'maxBytes': 10000000,
             'backupCount': 5,
         },
+        'file.handler.debug': {
+            'class': 'logging.handlers.RotatingFileHandler',
+            'formatter': 'standard',
+            'filename': os.path.join(config.Config.LOG_DIR, 'debug.log'),
+            'maxBytes': 10000000,
+            'backupCount': 5,
+        },
         'file.handler.request': {
             'class': 'logging.handlers.RotatingFileHandler',
             'formatter': 'standard',
@@ -174,6 +181,11 @@ LOGGING_CONFIG = {
         },
         'flask': {  # if __name__ == '__main__'
             'handlers': ['file.handler.flask'],
+            'level': 'DEBUG',
+            'propagate': False
+        },
+        'debug': {  # if __name__ == '__main__'
+            'handlers': ['file.handler.debug'],
             'level': 'DEBUG',
             'propagate': False
         },
